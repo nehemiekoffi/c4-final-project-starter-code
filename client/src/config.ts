345c24c4cee86eaa5658ea9aa89c65b1b3183cf1
Nehemie KOFFI <nehemiekoffi@gmail.com>
2022-11-16 19:43:44 +0000

    Update API KEY

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '7b2bd41aic'
+const apiId = 'rwcmlgspia'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
